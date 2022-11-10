62ef1abfc1034ac176bfc96bd0f2b3ae1a143758
Андрей <@DESKTOP-USER>
2022-11-10 12:09:57 +0400

    Добавьте файлы проекта.

---
+++ natural number 2/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Введите четырёхзначное натуральное число:");
+    var A = Convert.ToInt32(Console.ReadLine());
+
+    var result = A/10;
+    result%= 100;
+    Console.WriteLine("Двузначное число, состоящее из его средних цифр:");
+    Console.WriteLine(result);
+  }
+}
